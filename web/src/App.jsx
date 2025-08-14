f34ae88e1ffe867b1d7e992c0a87c56d7b4e3d6f
Aakash Shah <aaksha.shah94@gmail.com>
2025-08-14 00:10:13 -0700

    changin import order

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import Login from './pages/Login.jsx';
-import Dashboard from './pages/Dashboard.jsx';
 import SendKudos from './pages/SendKudos.jsx';
 import Redeem from './pages/Redeem.jsx';
 import AdminReport from './pages/AdminReport.jsx';
+import Dashboard from './pages/dashboard.jsx';
 
 export default function App() {
   return (
